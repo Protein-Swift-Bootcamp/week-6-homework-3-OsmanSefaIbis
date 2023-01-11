b3ac12f6b526718d495c59832fe900c540735b83
Sefa IBIS <osmansefaibis@gmail.com>
2023-01-11 15:03:28 +0300

    Initial Commit Boilerplate project

---
+++ ThirdHomework/App/Delegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ThirdHomework
+//
+//  Created by Sefa İbiş on 9.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ ThirdHomework/LandingVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ThirdHomework
+//
+//  Created by Sefa İbiş on 9.01.2023.
+//
+
+import UIKit
+
+class LandingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
